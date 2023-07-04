094bad431d5699ef1d6dc65a7869ab63154ccf9a
vcsil <vcsilva@poli.ufrj.br>
2023-07-04 16:44:04 -0300

    :sparkles: feat: pega o ofx do diretório atual e o do anterior imediato

--- comparaArqOFX.py
+++ comparaArqOFX.py
@@ -5,16 +5,41 @@ Created on Thu Apr 13 11:47:17 2023
 @author: vinic
 """
 
+import os
 from ofxparse import OfxParser
+
+
+def get_name_ofx(diretorio_atual):
+    for item in os.listdir(diretorio_atual):
+        if (".ofx" in item):
+            return item
+
+    raise FileNotFoundError(f'Ofx não está aqui: {diretorio_atual}')
+
+
+# Pega o diretório atual
+dir_atual = os.getcwd()
+list_dir_atual = dir_atual.split("\\")
+
+# Identifica o arquivo ofx no diretório atual
+ofx_atual = get_name_ofx(dir_atual)
+
+# Carrega o arquivo OFX atual
+with open(dir_atual + "\\" + ofx_atual, 'rb') as arquivo_ofx_atual:
+    ofx_atual = OfxParser.parse(arquivo_ofx_atual)
+
+# Vai buscar o arquivo ofx anterior
+# 1 - Busca no diretório anterior
+dir_superior = "\\".join(list_dir_atual[:-1])
+if (len(os.listdir(dir_superior)) > 1):
+    diretorio_anterior = os.listdir(dir_superior)[-2]
+    diretorio_anterior = dir_superior + "\\" + diretorio_anterior
+    ofx_anterior = get_name_ofx(diretorio_anterior)
 
 
 # Carrega o arquivo OFX antigo
 with open('./ofx-example/extratoITAU.ofx', 'rb') as arquivo_ofx_antigo:
     ofx_antigo = OfxParser.parse(arquivo_ofx_antigo)
-
-# Carrega o arquivo OFX atual
-with open('./ofx-example/extratoBB.ofx', 'rb') as arquivo_ofx_atual:
-    ofx_atual = OfxParser.parse(arquivo_ofx_atual)
 
 # Coleta todos os checknum (identificadores) do arquvio antigo
 list_checknum_antigo = []
@@ -28,4 +53,4 @@ len_transactions = len(todas_transactions_atual)
 for idx in range(len_transactions):
     if (todas_transactions_atual[idx].checknum in list_checknum_antigo):
         todas_transactions_atual.pop(idx)
-        idx -= 1;
+        idx -= 1
