dc6fd5b146badc1029d42c17364dc3ee5a87bc26
vcsil <vcsilva@poli.ufrj.br>
2023-07-04 16:46:19 -0300

    :recycle: refactor: função recursiva para identificar o ofx anterior

--- comparaArqOFX.py
+++ comparaArqOFX.py
@@ -12,45 +12,60 @@ from ofxparse import OfxParser
 def get_name_ofx(diretorio_atual):
     for item in os.listdir(diretorio_atual):
         if (".ofx" in item):
-            return item
+            return diretorio_atual + "\\" + item
 
     raise FileNotFoundError(f'Ofx não está aqui: {diretorio_atual}')
 
 
+def get_old_directory(diretorio_atual, diretorio_anterior="", nivel=-2):
+    if (diretorio_anterior == ""):
+        split_dir_atual = dir_atual.split("\\")
+        diretorio_anterior = "\\".join(split_dir_atual[:-1])
+
+    list_dir_anterior = os.listdir(diretorio_anterior)
+
+    # Verifica se tem mais de um arquivo no diretório, indica ter ofx,
+    # se não tiver, sobe até o anterior imediado
+    if (len(list_dir_anterior) == 1):
+        att_diretorio_anterior = "\\".join(diretorio_anterior.split("\\")[:-1])
+        return get_old_directory(diretorio_atual, att_diretorio_anterior, -2)
+    # Pastas que contém ofx são nomedas por até 3 dígitos númericos
+    elif (len(list_dir_anterior[nivel]) > 3):
+        att_diretorio_anterior = diretorio_anterior + "\\" + list_dir_anterior[nivel]
+        return get_old_directory(diretorio_atual, att_diretorio_anterior, -1)
+
+    else:
+        att_diretorio_anterior = diretorio_anterior + "\\" + list_dir_anterior[nivel]
+        return get_name_ofx(att_diretorio_anterior)
+
+
 # Pega o diretório atual
 dir_atual = os.getcwd()
-list_dir_atual = dir_atual.split("\\")
 
 # Identifica o arquivo ofx no diretório atual
 ofx_atual = get_name_ofx(dir_atual)
 
+# Vai buscar o arquivo ofx anterior imediado (da ultima data atualiazada)
+ofx_anterior = get_old_directory(dir_atual)
+
 # Carrega o arquivo OFX atual
-with open(dir_atual + "\\" + ofx_atual, 'rb') as arquivo_ofx_atual:
+with open(ofx_atual, 'rb') as arquivo_ofx_atual:
     ofx_atual = OfxParser.parse(arquivo_ofx_atual)
 
-# Vai buscar o arquivo ofx anterior
-# 1 - Busca no diretório anterior
-dir_superior = "\\".join(list_dir_atual[:-1])
-if (len(os.listdir(dir_superior)) > 1):
-    diretorio_anterior = os.listdir(dir_superior)[-2]
-    diretorio_anterior = dir_superior + "\\" + diretorio_anterior
-    ofx_anterior = get_name_ofx(diretorio_anterior)
+# Carrega o arquivo OFX anterior
+with open(ofx_anterior, 'rb') as arquivo_ofx_anterior:
+    ofx_anterior = OfxParser.parse(arquivo_ofx_anterior)
 
-
-# Carrega o arquivo OFX antigo
-with open('./ofx-example/extratoITAU.ofx', 'rb') as arquivo_ofx_antigo:
-    ofx_antigo = OfxParser.parse(arquivo_ofx_antigo)
-
-# Coleta todos os checknum (identificadores) do arquvio antigo
-list_checknum_antigo = []
-for transacao in ofx_antigo.account.statement.transactions:
-    list_checknum_antigo.append(transacao.checknum)
+# Coleta todos os checknum (identificadores) do arquvio anterior
+list_checknum_anterior = []
+for transacao in ofx_anterior.account.statement.transactions:
+    list_checknum_anterior.append(transacao.checknum)
 
 # Passar por todas as transações atuais
 todas_transactions_atual = ofx_atual.account.statement.transactions
 len_transactions = len(todas_transactions_atual)
 
 for idx in range(len_transactions):
-    if (todas_transactions_atual[idx].checknum in list_checknum_antigo):
+    if (todas_transactions_atual[idx].checknum in list_checknum_anterior):
         todas_transactions_atual.pop(idx)
         idx -= 1
